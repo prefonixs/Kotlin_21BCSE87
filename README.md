2317764bdb534195fdf3d31d0ded2c1fc148ec34
prefonixs <sidharthmishra10503@gmail.com>
2024-06-18 17:22:33 +0530

    UI Elements assignment

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # Text Assignment:
+
 <img src='https://github.com/prefonixs/Kotlin_21BCSE87/assets/138750805/eb3c92f0-88c7-41f1-b5eb-cee867c1a6c3' height=800 width=400>
 
 # Layout, Button and Dialog Assignment:
+
 <div style="display: flex; flex-direction: row;">
   <img src='https://github.com/prefonixs/Kotlin_21BCSE87/assets/138750805/dde99b48-99e3-4c2e-9f66-a8eb4d608e1b' height=800 width=400>
   <img src='https://github.com/prefonixs/Kotlin_21BCSE87/assets/138750805/e0c77d92-1517-434c-8827-2620a8a39088' height=800 width=400>
+</div>
+
+# Scaffold, Bottomsheets, Slider, Switch, Divider, Progress Indicator, Card, Chip Assignment:
+
+<div style="display: flex; flex-direction: row;">
+  <img src='https://github.com/prefonixs/Kotlin_21BCSE87/assets/138750805/29eee1aa-58ad-473b-9f28-bd54564209c8' height=800 width=400>
+  <img src='https://github.com/prefonixs/Kotlin_21BCSE87/assets/138750805/813cbd25-93b1-42be-8119-2a9206479f21' height=800 width=400>
 </div>
--- app/src/main/java/com/example/andbatch1/MainActivity.kt
+++ app/src/main/java/com/example/andbatch1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.andbatch1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,18 +9,39 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -33,6 +55,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.andbatch1.ui.theme.AndBatch1Theme
+import java.lang.Math.round
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalLayoutApi::class)
@@ -40,65 +63,96 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         val gColors = listOf(Color.Red, Color.Blue, Color.Green)
         setContent {
-            Column(modifier = Modifier
-                .fillMaxSize()
-                .padding(20.dp),
-                verticalArrangement = Arrangement.spacedBy(20.dp)) {
-
-                Text(text = "Layouts", fontWeight = FontWeight.Bold, fontSize = 25.sp)
-                Column() {
-                    Text("column Item 1")
-                    Text("column Item 2")
-                    Text("column Item 3")
+            ScaffoldExample()
+        }
+    }
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun ScaffoldExample(){
+        Scaffold(
+            topBar = {
+                TopAppBar(colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                ), title = { Text(text = "Top bar", fontSize = 30.sp) })
+            },
+            bottomBar = {
+                BottomAppBar(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    contentColor = MaterialTheme.colorScheme.primary
+                ) {
+                    Text(text = "Bottom bar")
                 }
-                Row() {
-                    Text("row Item 1")
-                    Text("row Item 2")
-                    Text("row Item 3")
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = { println("Edit is clicked") }) {
+                    Icon(Icons.Default.Edit, contentDescription = "Edit button")
                 }
-                Box() {
-                    Text("box Item Hello")
-                    Text("box Item World")
-                    Text("box Item Sidharth")
+            }
+        ) {
+            Column(modifier = Modifier.padding(it)) {
+                var showChip by remember {mutableStateOf(true)}
+                if (showChip) {
+                    AssistChip(
+                        modifier = Modifier.padding(10.dp),
+                        onClick = { showChip = false },
+                        label = { Text(text = "Chip") },
+                        leadingIcon = {
+                            Icon(
+                                Icons.Default.Close,
+                                contentDescription = "close"
+                            )
+                        })
                 }
-
-                Text(text = "Buttons", fontWeight = FontWeight.Bold, fontSize = 25.sp)
-                FlowRow( modifier = Modifier.width(1000.dp), horizontalArrangement = Arrangement.Center) {
-                    Button(onClick = { /*TODO*/ },modifier = Modifier.padding(8.dp)) {
-                        Text("Filled")
-                    }
-                    FilledTonalButton(onClick = { /*TODO*/ },modifier = Modifier.padding(8.dp)) {
-                        Text("Tonal")
-                    }
-                    OutlinedButton(onClick = { /*TODO*/ },modifier = Modifier.padding(8.dp)) {
-                        Text("Outlined")
-                    }
-                    ElevatedButton(onClick = { /*TODO*/ },modifier = Modifier.padding(8.dp)) {
-                        Text("Elevated")
-                    }
-                    TextButton(onClick = { /*TODO*/ },modifier = Modifier.padding(8.dp)){
-                        Text("Text Button")
-                    }
-                }
-
-                Text(text = "Dialog", fontWeight = FontWeight.Bold, fontSize = 25.sp)
-                var showDialog = remember { mutableStateOf(false) }
-
-                if (showDialog.value) {
-                    AlertDialog(
-                        onDismissRequest = { showDialog.value = false },
-                        title = { Text(text = "Title") },
-                        text = { Text("This is a dialog") },
-                        confirmButton = {
-                            Button(onClick = { showDialog.value = false }) {
-                                Text("OK")
-                            }
-                        }
+                Card (colors = CardDefaults.cardColors(containerColor = Color.Green), modifier = Modifier
+                    .size(width = 250.dp, height = 200.dp)
+                    .padding(10.dp)){
+                    Text(
+                        text = "Card Title",
+                        fontWeight = FontWeight.ExtraBold,
+                        fontSize = 25.sp,
+                        modifier = Modifier.padding(15.dp),
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = "Name : Sidharth Mishra",
+                        fontSize = 20.sp,
+                        modifier = Modifier.padding(10.dp),
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = "SIC: 21BCSE87",
+                        fontSize = 20.sp,
+                        modifier = Modifier.padding(10.dp),
+                        textAlign = TextAlign.Center
                     )
                 }
-
-                Button(onClick = { showDialog.value = true }) {
-                    Text("Show Dialog")
+                var sliderVal by remember {mutableStateOf(0f)}
+                Slider(
+                    modifier = Modifier.padding(10.dp),
+                    value = sliderVal,
+                    onValueChange = { sliderVal = it },
+                    steps=9,
+                    valueRange = 0f..100f
+                )
+                Text(text = "value : ${round(sliderVal)}",modifier = Modifier.padding(10.dp))
+                Divider(thickness = 3.dp, color = Color.Gray)
+                CircularProgressIndicator(modifier = Modifier.padding(10.dp))
+                var checked by remember {
+                    mutableStateOf(true)
+                }
+                Switch(checked = checked, onCheckedChange = {checked=it},modifier = Modifier.padding(10.dp))
+                var showBottomSheet by remember {mutableStateOf(false)}
+                Button(onClick = {showBottomSheet=true}) {
+                    Text(text = "Show BottomSheet")
+                }
+                if(showBottomSheet){
+                    ModalBottomSheet(onDismissRequest = {showBottomSheet=false},modifier = Modifier.fillMaxSize()) {
+                        Text(text = "Bottom Sheet",
+                            fontSize = 20.sp,
+                            modifier = Modifier.padding(10.dp),
+                            textAlign = TextAlign.Center)
+                    }
                 }
             }
         }
